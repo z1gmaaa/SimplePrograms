3c531936470fb8cee634c87d2b92d17a0c1b193b
z1gmaaa <93038113+z1gmaaa@users.noreply.github.com>
2021-10-23 17:16:19 +0530

    Create sum_of_two_numbers.cpp

---
+++ sum_of_two_numbers.cpp
@@ -0,0 +1,16 @@
+//This progam is for finding the sum of two numbers.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num_a,num_b;
+    cout<<"input A :";
+    cin>>num_a;
+    cout<<"input B :";
+    cin>>num_b;
+    cout<<"Sum of A and B is:"<<num_a+num_b;
+    
+
+    return 0;
+}
